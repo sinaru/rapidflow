464ac5f432459f8223f84f3dae6f41f676531ec1
Sinaru Gunawardena <sinarug@gmail.com>
2025-11-11 10:53:26 +0000

    Fix file name for batch builder

--- lib/rapidflow.rb
+++ lib/rapidflow.rb
@@ -6,7 +6,7 @@ require_relative "rapidflow/counter"
 require_relative "rapidflow/pipeline"
 require_relative "rapidflow/work_item"
 require_relative "rapidflow/stage"
-require_relative "rapidflow/builder_builder"
+require_relative "rapidflow/batch_builder"
 require_relative "rapidflow/batch"
 
 module RapidFlow
